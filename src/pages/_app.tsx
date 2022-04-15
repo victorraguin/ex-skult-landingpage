5bfe825709b7225e98768b1ee23aff2b5618e5d6
Victor Raguin <victor.raguin@live.fr>
2022-04-15 07:53:46 +0200

    fix: build aws correction with notistack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
-import { SnackbarProvider } from 'notistack';
 import type { AppProps } from 'next/app';
+import Home from './index';
 
 //test
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <SnackbarProvider maxSnack={3}>
-      <Component {...pageProps} />
-    </SnackbarProvider>
-  );
+function MyApp() {
+  return <Home />;
 }
 export default MyApp;
