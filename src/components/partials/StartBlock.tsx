cb4c209516fe3d51fb42f7729c293c4eb9e23ded
Jeremie <jeremiegthr@gmail.com>
2022-05-30 11:58:38 +0200

    WIP: victor

--- src/components/partials/StartBlock.tsx
+++ src/components/partials/StartBlock.tsx
@@ -3,55 +3,61 @@
 import React from 'react';
 import styles from './Landing.module.css';
 import { motion } from 'framer-motion';
+import DiscordButton from './images/DiscordButton';
+import Link from 'next/link';
 
 const StartBlock = () => {
   return (
     <div
-      className="flex flex-col bg-[#222222] bg-cover bg-center bg-no-repeat"
+      className="bg-[#222222] bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: `url("/elements/bckground-1.webp")`,
       }}
     >
-      <div
-        id="top"
-        className="relative px-10 pb-[18rem] md:py-10 xl:px-[10%] xl:py-60 2xl:px-[18%] 3xl:px-[18%] 3xl:pb-[25rem] 3xl:pt-[15rem] 4xl:px-[25%]"
-      >
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1, duration: 1 }}>
-          <h1 className={`${styles.HomeTitle} pb-3 leading-none`}>Let's go</h1>
-        </motion.div>
-        <div className="xl:w-1/2">
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.5, duration: 1 }}>
-            <h1 className={`${styles.HomeLittleTitle}`}>beyond Gameplay</h1>
-          </motion.div>
-        </div>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 2, duration: 1 }}>
-          <div className="mt-2 font-poppins text-[18px] font-light text-[#e7e7e7] xl:text-[24px]">
-            Streams can be much more than just a video!
+      <div className="flex flex-row flex-wrap">
+        <div className="flex  flex-col">
+          <div id="top" className="relative md:py-10 xl:py-60 3xl:pb-[25rem] 3xl:pt-[15rem]">
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1, duration: 1 }}>
+              <h1 className={`${styles.HomeTitle} pb-3 leading-none`}>Let's go</h1>
+            </motion.div>
+            <div className="xl:w-1/2">
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.5, duration: 1 }}>
+                <h1 className={`${styles.HomeLittleTitle}`}>beyond Gameplay</h1>
+              </motion.div>
+            </div>
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 2, duration: 1 }}>
+              <div className="mt-2 font-poppins text-[18px] font-light text-[#e7e7e7] xl:text-[24px]">
+                Streams can be much more than just a video!
+              </div>
+            </motion.div>
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 3, duration: 1 }}>
+              <a href="https://discord.gg/fJvrQCJr4W" target="_blank" rel="noopener noreferrer">
+                <motion.button
+                  className={`mt-4 scale-[0.6] rounded-full bg-gradient-to-br from-[#FFCC00] to-[#ED713C] p-0.5 text-sm hover:scale-[0.65] md:scale-[0.7] md:hover:scale-[0.75] xl:scale-[0.8] xl:hover:scale-[0.85] 2xl:scale-[0.9] 2xl:hover:scale-[0.95] 3xl:scale-[0.95] 3xl:hover:scale-[1] 4xl:hover:scale-[1.05]`}
+                >
+                  <motion.div className="group relative flex flex-row rounded-full bg-[#262626] px-6 pt-2">
+                    <DiscordButton fill="#fff" />
+                    <motion.div
+                      className={`pt-1.5 pl-2 font-poppins text-[20px] font-light text-[#a9a9a9] group-hover:brightness-150`}
+                    >
+                      Join the Discord
+                    </motion.div>
+                  </motion.div>
+                </motion.button>
+              </a>
+            </motion.div>
           </div>
-        </motion.div>
+        </div>
+        <motion.img
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 2, duration: 0.5 }}
+          src="/skult-streamer-cards.png"
+          alt="SKULT Streamer Cards"
+          className="my-auto mx-auto h-[210px] w-[321px]"
+        />
       </div>
       <img src="/elements/header-footer.webp" alt="Header Footer" className="relative z-20 w-screen" />
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 2, duration: 0.5 }}>
-        <motion.img
-          src="./elements/skyrroz.svg"
-          alt="Skyrroz"
-          className="absolute top-[14rem] right-[3.5rem] z-30 h-80 scale-50 transition duration-200 ease-in-out md:top-[1rem] md:scale-[0.6] lg:top-0 xl:top-60 xl:right-60 xl:scale-100 2xl:top-[14rem] 2xl:right-[20rem] 3xl:top-[18rem] 3xl:right-[31rem] 4xl:right-[39rem]"
-        />
-      </motion.div>
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 2.5, duration: 0.5 }}>
-        <img
-          src="./elements/natasha.svg"
-          alt="Natasha"
-          className="absolute top-[15rem] right-[-1rem] z-20 h-80  rotate-[18deg] scale-50 transition duration-200 ease-in-out md:top-[2rem] md:scale-[0.6] xl:top-60 xl:right-[8rem] xl:scale-100 2xl:right-[13rem] 3xl:top-[20rem] 3xl:right-[23rem] 4xl:right-[31rem]"
-        />
-      </motion.div>
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 3, duration: 0.5 }}>
-        <img
-          src="./elements/waartex.svg"
-          alt="Waartex"
-          className="absolute top-[16rem] right-[8rem] z-10 h-80 rotate-[350deg] scale-50 transition duration-200 ease-in-out md:top-[2rem] md:scale-[0.6] xl:top-60 xl:right-[22rem] xl:scale-100 2xl:right-[28rem] 3xl:top-[21rem] 3xl:right-[39rem] 4xl:right-[47rem]"
-        />
-      </motion.div>
     </div>
   );
 };
