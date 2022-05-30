1a95cc49315bda904b1c39bef3f8f2b52747fc3d
Jeremie <jeremiegthr@gmail.com>
2022-05-30 10:56:36 +0200

    fix: hover effect

--- src/components/partials/Header.tsx
+++ src/components/partials/Header.tsx
@@ -25,13 +25,27 @@ const Header = () => {
             />
           </Link>
         </motion.div>
-        <motion.div initial={{ y: -250 }} animate={{ y: -10 }} transition={{ delay: 1, type: 'spring', stiffness: 70 }}>
+        <motion.div
+          initial={{ y: -250 }}
+          animate={{ y: -10 }}
+          transition={{ delay: 1, type: 'spring', stiffness: 70 }}
+          className="group"
+        >
           <Link href="/streamer" passHref>
             <motion.button
-              className={`${styles.HeaderJoinDiscord} absolute top-[-2.6rem] right-[-2rem] scale-[0.6] rounded-full bg-gradient-to-br from-[#FFCC00] to-[#ED713C] p-0.5 text-sm hover:scale-[0.65] md:right-[0rem] md:scale-[0.7] md:hover:scale-[0.75]  xl:top-[-3rem] xl:right-20 xl:scale-[0.8] xl:hover:scale-[0.85] 2xl:scale-[0.9] 2xl:hover:scale-[0.95] 3xl:scale-[0.95] 3xl:hover:scale-[1] 4xl:scale-[1] 4xl:hover:scale-[1.05]`}
+              className={`
+                ${styles.HeaderJoinDiscord} absolute top-[-2.6rem] right-[-2rem] scale-[0.6] rounded-full
+                bg-gradient-to-br from-[#FFCC00] to-[#ED713C] p-0.5 text-sm
+                group-hover:scale-[0.65]
+                md:right-[0rem] md:scale-[0.7] md:group-hover:scale-[0.75]
+                xl:top-[-3rem] xl:right-20 xl:scale-[0.8] xl:group-hover:scale-[0.85]
+                2xl:scale-[0.9] 2xl:group-hover:scale-[0.95]
+                3xl:scale-[0.95] 3xl:group-hover:scale-[1]
+                4xl:scale-[1] 4xl:group-hover:scale-[1.05]
+              `}
             >
-              <motion.div className="relative flex flex-row rounded-full bg-[#262626] px-6 py-[8px] hover:brightness-150">
-                <motion.div className="py-[6px] font-poppins text-[20px] font-light text-[#a9a9a9]">
+              <motion.div className="relative flex flex-row rounded-full bg-[#262626] px-6 py-[8px]">
+                <motion.div className="py-[6px] font-poppins text-[20px] font-light text-[#a9a9a9] group-hover:text-white">
                   I&apos;m a Streamer
                 </motion.div>
               </motion.div>
