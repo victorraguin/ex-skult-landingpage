c31217b94cc51d56bfb00226750e970dc80a2d4a
Victor Raguin <victor.raguin@live.fr>
2022-06-17 10:55:49 +0200

    fix: streamer rank

--- src/components/partials/StreamerRanks.tsx
+++ src/components/partials/StreamerRanks.tsx
@@ -4,25 +4,22 @@ import Image from 'next/image';
 export const StreamerRanks = () => (
   <div className="flex flex-col gap-y-28 pt-10 pb-32 text-lg 2xl:text-xl">
     <Title />
-    <p className="mx-auto max-w-xs text-center font-medium text-[#e7e7e7] md:max-w-xl">
+    <p className="mx-auto max-w-xs text-center text-2xl font-medium text-[#e7e7e7] md:max-w-xl">
       Streamer Ranks based on your popularity
-      <br />
-      For every card sold you get:
     </p>
 
     <div className="mx-auto flex w-full max-w-7xl flex-row flex-wrap items-center justify-evenly gap-y-8">
-      {ranks.map(({ icon, name, royalties }) => (
+      {ranks.map(({ icon, name }) => (
         <div key={name} className="flex flex-col items-center">
           <Image src={icon} layout="fixed" width={200} height={100} />
           <p className="text-xl font-semibold text-[#f3bf00] 2xl:text-2xl">{name}</p>
-          <p>
-            <span className="font-medium text-[#989898]">Get</span> {royalties}%
-          </p>
         </div>
       ))}
     </div>
-
     <p className="mx-auto max-w-xs text-center md:max-w-xl">
+      <span className=" text-2xl text-primary">
+        The more you rank, the more you earn. <br />
+      </span>
       Our games are designed for any Streamer to get an audience and for any Player to get rewarded
     </p>
   </div>
@@ -38,16 +35,13 @@ const ranks = [
   {
     icon: 'https://cdn.skult.gg/Landing+Page/1stars.png',
     name: 'ROOKIE',
-    royalties: 10,
   },
   {
     icon: 'https://cdn.skult.gg/Landing+Page/2stars.png',
     name: 'HERO',
-    royalties: 15,
   },
   {
     icon: 'https://cdn.skult.gg/Landing+Page/3stars.png',
     name: 'LEGEND',
-    royalties: 25,
   },
 ];
