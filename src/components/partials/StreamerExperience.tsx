db6c29749c2c5313950be912cf6f6acbebcdfd7d
Jeremie <jeremiegthr@gmail.com>
2022-07-27 16:14:22 +0200

    feat: streamers trad

--- src/components/partials/StreamerExperience.tsx
+++ src/components/partials/StreamerExperience.tsx
@@ -1,44 +1,44 @@
+import { Trans, useTranslation } from 'next-i18next';
 import Image from 'next/image';
 import { GradientText } from './GradientText';
 
-export const StreamerExperiences = () => (
-  <div className="flex min-h-[70vh] flex-1 bg-[#222222] pb-20">
-    <div
-      className={`
+export const StreamerExperiences = () => {
+  const { t } = useTranslation('streamers');
+
+  return (
+    <div className="flex min-h-[70vh] flex-1 bg-[#222222] pb-20">
+      <div
+        className={`
         m-auto flex max-w-xs flex-1 flex-col items-center justify-around
         gap-y-16 gap-x-8 md:max-w-xl md:flex-row lg:max-w-7xl lg:gap-x-0
       `}
-    >
-      <Experience
-        icon="https://cdn.skult.gg/Landing+Page/EVENTS.png"
-        title={
-          <span>
-            Create <GradientText className="font-semibold">exclusive</GradientText> events
-          </span>
-        }
-        description={`
-          We will provide you with the toolbox to generate unique events for your community.
-          YOU decide who can join the events that YOU created!
-          Reward you loyaliest fans with engaging and interactive experiences.
-        `}
-      />
-      <Experience
-        icon="https://cdn.skult.gg/Landing+Page/CARD-PLAY-EARN.png"
-        title={
-          <span>
-            Play & <GradientText className="font-semibold">Earn</GradientText>
-          </span>
-        }
-        description={`
-          Let your audience play games while you're streaming!
-          Our first game: Fantasy Leagues!
-          Entertain your community with your in-game actions and boost their score with crazy performances!
-          Show them who's the best!
-        `}
-      />
+      >
+        <Experience
+          icon="https://cdn.skult.gg/Landing+Page/EVENTS.png"
+          title={
+            <Trans i18nKey="experience.events.title" ns="streamers">
+              <span>
+                Create <GradientText className="font-semibold">exclusive</GradientText> events
+              </span>
+            </Trans>
+          }
+          description={t('experience.events.description')}
+        />
+        <Experience
+          icon="https://cdn.skult.gg/Landing+Page/CARD-PLAY-EARN.png"
+          title={
+            <Trans i18nKey="experience.title" ns="streamers">
+              <span>
+                Play & <GradientText className="font-semibold">Earn</GradientText>
+              </span>
+            </Trans>
+          }
+          description={t('experience.earn.description')}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const Experience = ({ icon, title, description }: { icon: string; title: React.ReactNode; description: string }) => (
   <div className="flex max-w-sm flex-1 flex-col items-center gap-y-4 2xl:gap-y-16">
