7ceccc98b0496969c6bf037c4d57bb80bc101ae6
Jeremie <jeremiegthr@gmail.com>
2022-07-28 13:15:01 +0200

    fix: next config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,12 @@
 const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.skult.gg'],
   },
   i18n,
 };
+
+module.exports = nextConfig;
