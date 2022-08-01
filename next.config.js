3c47a3e9e1cb4d206a82d260cc690959ddbfa447
Victor Raguin <victor.raguin@live.fr>
2022-08-01 16:51:46 +0200

    fix:(aws deployment): change i18n config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /* eslint-disable unicorn/prefer-module */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { i18n } = require('./next-i18next.config');
+// const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.skult.gg'],
+    
   },
-  i18n,
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+  },
 };
 
 module.exports = nextConfig;
